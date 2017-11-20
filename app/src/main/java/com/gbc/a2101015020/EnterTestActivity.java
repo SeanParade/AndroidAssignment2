27c0b6207b5527e2023d6a8c1039c9019a524a5a
seanparade <seanparade@gmail.com>
2017-11-20 12:41:16 -0500

    changed onclick listener for enter test to be programatic.

--- app/src/main/java/com/gbc/a2101015020/EnterTestActivity.java
+++ app/src/main/java/com/gbc/a2101015020/EnterTestActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class EnterTestActivity extends Activity {
@@ -16,6 +17,7 @@ public class EnterTestActivity extends Activity {
     EditText bpl;
     EditText bph;
     EditText temperature;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +29,19 @@ public class EnterTestActivity extends Activity {
         bpl = findViewById(R.id.bpl_edit);
         bph = findViewById(R.id.bph_edit);
         temperature = findViewById(R.id.temperature_edit);
+        button = findViewById(R.id.submit_test_button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                // TODO: 20/11/2017 Save data to database
+                Log.d(TAG, "Enter Test: Pressed");
+                //AppDatabase.destroyInstance();
+                startActivity(new Intent(view.getContext(), MainMenuActivity.class));
+            }
+        });
 
     }
 
-    protected void onEnterTestClick(View view){
-        Log.d(TAG, "Enter Test: Pressed");
-        //AppDatabase.destroyInstance();
-        startActivity(new Intent(view.getContext(), MainMenuActivity.class));
-    }
+
 }
