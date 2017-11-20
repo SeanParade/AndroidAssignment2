0a53b341b54949b549f373e4b670bc04943c6fe6
seanparade <seanparade@gmail.com>
2017-11-20 12:57:36 -0500

    added rec view adapter

---
+++ app/src/main/java/com/gbc/a2101015020/UserAdapter.java
@@ -0,0 +1,32 @@
+package com.gbc.a2101015020;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created on 11/20/2017.
+ */
+
+class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/gbc/a2101015020/ViewTestsActivity.java
+++ app/src/main/java/com/gbc/a2101015020/ViewTestsActivity.java
@@ -2,12 +2,22 @@ package com.gbc.a2101015020;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class ViewTestsActivity extends Activity {
+
+    RecyclerView recView;
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_tests);
+
+        recView = findViewById(R.id.test_recycler_view);
+        recView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UserAdapter();
+        recView.setAdapter(adapter);
     }
 }
