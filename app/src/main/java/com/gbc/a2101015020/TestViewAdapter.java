9b166a9ff6f68a6c88408a3005ab771996987c74
Sean Price <seanparade@gmail.com>
2017-11-27 18:08:14 -0500

    working on listview

--- app/src/main/java/com/gbc/a2101015020/TestViewAdapter.java
+++ app/src/main/java/com/gbc/a2101015020/TestViewAdapter.java
@@ -1,68 +1,47 @@
 package com.gbc.a2101015020;
 
-import android.support.v7.widget.RecyclerView;
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
+import com.gbc.a2101015020.db.Test;
 import java.util.List;
 
 /**
  * Created on 11/20/2017.
  */
 
-public class RecViewAdapter extends RecyclerView.Adapter<RecViewAdapter.ViewHolder>{
-
-    public interface OnItemClickListener {
-        void onItemClick(String dataItem);
-    }
-
-    private final ArrayList<String> data;
-    private final OnItemClickListener listener;
-
-    public RecViewAdapter(ArrayList<String> data, OnItemClickListener listener) {
-        this.data = data;
-        this.listener = listener;
+public class TestViewAdapter extends ArrayAdapter<Test>{
+    public TestViewAdapter(@NonNull Context context, List<Test> tests) {
+        super(context, 0, tests);
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.test_row, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.bind(data.get(position), listener);
-    }
-
-    @Override
-    public int getItemCount() {
-        return data.size();
-    }
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
-
-        private TextView cell;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            cell = itemView.findViewById(R.id.rec_cell);
+    public View getView(int pos, View convertView, ViewGroup parent){
+        Test test = getItem(pos);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.test_row, parent, false);
         }
 
-        public void bind (final String data, final OnItemClickListener listener) {
-            cell.setText(data);
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    listener.onItemClick(data);
-                }
-            });
-        }
+        TextView testIdCell =  convertView.findViewById(R.id.test_id_cell);
+        TextView patientNameCell = convertView.findViewById(R.id.patient_cell);
+        TextView bphCell = convertView.findViewById(R.id.bph_cell);
+        TextView bplCell = convertView.findViewById(R.id.bpl_cell);
+        TextView tempCell = convertView.findViewById(R.id.temp_cell);
+
+        testIdCell.setText(String.valueOf(test.getTestId()));
+        patientNameCell.setText(String.valueOf(test.getPatientId()));
+        bphCell.setText(String.valueOf(test.getBph()));
+        bplCell.setText(String.valueOf(test.getBpl()));
+        tempCell.setText(String.valueOf(test.getTemperature()));
+
+        return convertView;
+
     }
-
-
 }
+
+
+
--- app/src/main/java/com/gbc/a2101015020/ViewTestsActivity.java
+++ app/src/main/java/com/gbc/a2101015020/ViewTestsActivity.java
@@ -1,24 +1,19 @@
 package com.gbc.a2101015020;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.Toast;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import com.gbc.a2101015020.db.AppDatabase;
 import com.gbc.a2101015020.db.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class ViewTestsActivity extends Activity {
 
-    RecyclerView recView;
-    RecyclerView.Adapter adapter;
+    ListView listView;
+    ArrayAdapter<Test> adapter;
     ArrayList<String> tests;
     AppDatabase mDb;
 
@@ -26,20 +21,13 @@ public class ViewTestsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_tests);
-        recView = findViewById(R.id.test_recycler_view);
-        mDb = AppDatabase.getDatabase(getApplicationContext());
 
-        tests = new ArrayList<>();
-        fetchTests(); // populate tests
-        recView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new RecViewAdapter(tests, new RecViewAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(String dataItem) {
-                String testID = dataItem.substring(dataItem.length()-7,dataItem.length()-1);
-                Toast.makeText(ViewTestsActivity.this, "item clicked" + testID, Toast.LENGTH_SHORT).show();
-            }
-        });
-        recView.setAdapter(adapter);
+        listView = findViewById(R.id.test_list_view);
+        mDb = AppDatabase.getDatabase(getApplicationContext());
+        adapter = new ArrayAdapter<>(
+                this, R.layout.test_row, mDb.testDao().getAll());
+        TestViewAdapter tAdapter = new TestViewAdapter(this, mDb.testDao().getAll());
+        listView.setAdapter(tAdapter);
     }
 
     private void fetchTests() {
